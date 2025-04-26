ea368620f6be571446f609245c51753602edc176
liamgines <109954426+liamgines@users.noreply.github.com>
2025-04-26 00:19:21 -0700

    Add Express.js as a dependency, set up basic routes that users can access

--- README.md
+++ README.md
@@ -9,11 +9,12 @@ cd project_root
 Then create a ".env" file containing:
 ```
 DATABASE_CONNECTION_STRING=type_string_here
+SERVER_PORT=type_server_port_number_here
 ```
 Finally, install dependencies:
 ```
 npm init -y
-npm install pg dotenv
+npm install pg express dotenv
 ```
 
 # Run
---
+++ main.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const { Client } = require("pg");
+const client = new Client({
+    connectionString: process.env.DATABASE_CONNECTION_STRING,
+});
+
+
+async function main() {
+    app.use(express.json());
+
+    app.get("/", (request, response) => {
+        response.send("Recipe Book");
+    });
+
+    app.get("/users", async(request, response) => {
+        try {
+            await client.connect();
+            const result = await client.query("SELECT * FROM users");
+            response.json(result.rows);
+            await client.end();
+        } catch (error) {
+            console.error(error);
+            response.status(500).send("Server Error");
+        }
+    });
+
+    const SERVER_PORT = process.env.SERVER_PORT;
+    app.listen(SERVER_PORT, () => {
+        console.log(`Server running on http://localhost:${SERVER_PORT}`);
+    });
+}
+
+main().catch(console.error);
