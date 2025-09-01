599fd3993749557c3a455e2dab4c3ee438566460
liamgines <109954426+liamgines@users.noreply.github.com>
2025-09-01 15:20:10 -0700

    Refactor entry point

--- main.js
+++ main.js
@@ -1,83 +1,54 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const path = require("node:path");
+const { Pool } = require("pg");
+
+const CURRENT_WORKING_DIRECTORY = __dirname;
+const PROJECT_TITLE = "Recipe Book";
+
+dotenv.config({ path: `${CURRENT_WORKING_DIRECTORY}/.env` });
+const SERVER_PORT = process.env.SERVER_PORT;
+
 const app = express();
 app.set("view engine", "ejs");
-/* Explicitly specify "views" path */
 app.set("views", "./views");
-/* Serve static files (HTML, CSS, Javascript, etc.) */
 app.use(express.static("public"));
 
-require("dotenv").config();
-const { Client } = require("pg");
-const client = new Client({
-    connectionString: process.env.DATABASE_CONNECTION_STRING,
+const pool = new Pool({ connectionString: process.env.DATABASE_CONNECTION_STRING });
+
+app.get("/", (request, response) => {
+    response.render("index", { browserTitle: PROJECT_TITLE, pageTitle: PROJECT_TITLE });
 });
 
-async function main() {
-    // Built-in middleware
-    app.use(express.json());
-    await client.connect();
+app.get("/users", async (request, response) => {
+    const result = await pool.query("SELECT * FROM users");
+    response.render("users", { users : result.rows });
+});
 
-    app.get("/", (req, res) => {
-        res.render("index", { 
-            browserTitle: "Recipe Book", 
-            pageTitle: "Recipe Book" 
-        });
-    });
+app.get("/recipes", async (request, response) => {
+    response.render("recipes");
+});
 
-    app.get("/users", async(request, response) => {
-        try {
-            // result.rows format = [json_object1, json_object2, ...]
-            // json_object format = {"id": 1, "name": "string"}
-            const result = await client.query("SELECT * FROM users");
-            // response.json(result.rows);
-            response.render("users", {users : result.rows});
-        } catch (error) {
-            console.error(error);
-            response.status(500).send("Server Error");
-        }
-    });
+app.post("/api/recipes", async (request, response) => {
+    const recipe = request.body;
+    await pool.query(`INSERT INTO RECIPES (NAME, DESCRIPTION, INGREDIENTS, COOK_TIME, TAGS) VALUES ($1, $2, $3, $4, $5)`, [recipe.name, recipe.description, recipe.ingredients, recipe.cookTime, recipe.tags]);
+    response.send(recipe);
+});
 
-    app.get("/recipes", async(request, response) => {
-        try {
-            /*
-            const result = await client.query("SELECT * FROM recipes");
-            response.render("recipes", {recipes : result.rows});
-            */
-            response.render("recipes");
-        } catch (error) {
-            console.error(error);
-            response.status(500).send("Server Error");
-        }
-    });
+app.get("/api/recipes", async (request, response) => {
+    const result = await pool.query("SELECT * FROM recipes");
+    response.send(result.rows);
+});
 
-    app.post("/api/recipes", async(request, response) => {
-        try {
-            // console.log("request", request.body);
-            recipe = request.body;
-            console.log(recipe);
-            await client.query(`INSERT INTO RECIPES (NAME, DESCRIPTION, INGREDIENTS, COOK_TIME, TAGS) VALUES ($1, $2, $3, $4, $5)`, [recipe.name, recipe.description, recipe.ingredients, recipe.cookTime, recipe.tags]);
-            response.send(request.body);
-        } catch (error) {
-            console.error(error);
-            response.status(500).send("Server Error");
-        }
-    });
+app.use((error, request, response, next) => {
+    console.error(error);
+    response.status(500).send("Server Error");
+});
 
-    app.get("/api/recipes", async(request, response) => {
-        try {
-            const result = await client.query("SELECT * FROM recipes");
-            response.send(result.rows);
-        } catch (error) {
-            console.error(error);
-            response.status(500).send("Server Error");
-        }
-    });
-
-    const SERVER_PORT = process.env.SERVER_PORT;
-    app.listen(SERVER_PORT, () => {
-        console.log(`Server running on http://localhost:${SERVER_PORT}`);
-    });
-    // await client.end();
-}
-
-main().catch(console.error);
+app.listen(SERVER_PORT, (error) => {
+    if (error) {
+        throw error;
+    }
+    console.log(`App listening on port ${SERVER_PORT}`);
+    pool.connect();
+});
