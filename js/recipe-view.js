40d188667f5590e83720066d3bd315f6ca492b22
Ty <168314093+tyreseGill@users.noreply.github.com>
2025-09-12 12:38:47 -0700

    feat(recipe-view): display recipe details from TheMealDB API by ID in URL - Implemented logic to parse `?id={recipeId}` from  recipe-view.html url query - Fetched recipe data from TheMealDB API using the  provided recipe ID - Displayed recipe thumbnail, publisher, and link to original recipe source - Embedded video tutorial when available - Rendered walkthrough steps for recipe preparation

--- js/recipe-view.js
+++ js/recipe-view.js
@@ -2,52 +2,156 @@ const navigateWalkthroughContainer = document.getElementById("navigate-walkthrou
 const stepFractionPresentation = document.getElementById("completion-fraction");
 const progressBar = document.getElementById("completion-progress");
 const stepCounter = document.getElementById("step-counter");
+const currentUrl = window.location.href
+const [ _, recipeApiId ] = currentUrl.split("?id=");
 
-let [ getPreviousStep, getNextStep ] = navigateWalkthroughContainer.getElementsByTagName("button");
-let { currentStep, lastStep } = getSteps();
+const usernameElement = document.getElementById("username");
+const usernameAndDateElement = document.getElementById("username-and-date");
+const dateElement = document.getElementById("publishing-date");
+const separatorElement = document.getElementById("separator");
 
-progressBar.value = currentStep / lastStep;
-getPreviousStep.style.cursor = "not-allowed";
-getPreviousStep.disabled = true;
+const usernameLink = document.getElementById("username-link");
+const websiteLink = document.getElementById("origin-site-link");
+const websiteAvailableImg = document.getElementById("origin-site-available");
+const websiteNotAvailableImg = document.getElementById("origin-site-not-available")
+
+const recipeName = document.getElementById("recipe-name");
+const username = document.getElementById("username-str");
+const thumbnail = document.getElementById("recipe-thumbnail");
+const videoElement = document.getElementById("recipe-tutorial");
+const instructionsTextElement = document.getElementById("instructions");
+
+const ingredientListElement = document.getElementById("ingredient-list");
 
 function getSteps() {
-    let [ currentStep, lastStep ] = stepFractionPresentation.textContent.split(" / ");
+    let [ currentStep, _ ] = stepFractionPresentation.textContent.split(" / ");
     currentStep = parseInt(currentStep);
+    lastStep = instructions.length;
     return { currentStep, lastStep};
 }
 
-getPreviousStep.addEventListener("click", () => {
+async function searchForRecipe() {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeApiId}`);
+    if (!response.ok) {
+        throw "Error occurred fetching data!";
+    }
+    const data = await response.json();
+    return data["meals"][0];
+}
+
+function listPublisher(url, publishingDate) {
+    urlCopy = url;
+    if (!url) {
+        domainName = "AnonymousPublisher";
+        websiteAvailableImg.style.display = "none";
+    }
+    else {
+        const urlComponentsToRemove = ["https://", "http://", "www."];
+        for (let i = 0; i < urlComponentsToRemove.length; i++){
+            urlCopy = urlCopy.replace(urlComponentsToRemove[i], "");
+        }
+
+        startIndex = 0;
+        endIndex = urlCopy.indexOf(".");
+        domainName = urlCopy.substring(startIndex, endIndex);
+
+        usernameLink.href = "public-account.html";
+        websiteNotAvailableImg.style.display = "none";
+        websiteLink.href = url;
+    }
+    usernameElement.textContent = domainName;
+
+    if (publishingDate){
+        dateElement.textContent = `Uploaded on ${publishingDate}`;  
+    }
+    else {
+        separatorElement.remove();
+        dateElement.remove();
+    }
+}
+
+function getIngredients(recipe) {
+    let ingredients = [];
+    let measurements = [];
+
+    for (let i=1; i <= 20; i++) {
+        ingredient = recipe[`strIngredient${i}`]
+        measurement = recipe[`strMeasure${i}`]
+        if (ingredient) {
+            ingredients.push(ingredient);
+            measurements.push(measurement)
+        }
+        else {
+            return {ingredients, measurements}
+        }
+    }
+
+    return {ingredients, measurements}
+}
+
+async function fillRecipeViewPage() {
+    const recipe = await searchForRecipe();
+    recipeName.textContent = recipe["strMeal"]
+    listPublisher(recipe["strSource"], recipe["dateModified"]);
+    thumbnail.src = recipe["strMealThumb"];
+    instructions = recipe["strInstructions"].replace(/\r/g, "").split("\n").filter(str => str !== "").filter(str => str.length > 7)
+    let [_, tutorialVideoId] = recipe["strYoutube"].split("?v=");
+    videoElement.setAttribute("src", `https://www.youtube.com/embed/${tutorialVideoId}`)
+    instructionsTextElement.textContent = instructions[0];
+    let {ingredients, measurements} = getIngredients(recipe);
+    return { instructions }
+}
+
+async function main() {
+    ({instructions} = await fillRecipeViewPage());
+    let [ getPreviousStep, getNextStep ] = navigateWalkthroughContainer.getElementsByTagName("button");
     let { currentStep, lastStep } = getSteps();
 
-    if (currentStep != 1) {
-        currentStep -= 1;
-        stepFractionPresentation.textContent = `${currentStep} / ${lastStep}`;
-        stepCounter.textContent = currentStep;
-        progressBar.value = currentStep / lastStep;
 
-        if (currentStep == 1) {
-            getPreviousStep.style.cursor = "not-allowed";
-            getPreviousStep.disabled = true;
+    progressBar.value = currentStep / lastStep;
+    getPreviousStep.style.cursor = "not-allowed";
+    getPreviousStep.disabled = true;
+    getNextStep.style.cursor = "pointer"
+
+    stepFractionPresentation.textContent = `1 / ${instructions.length}`
+
+    getPreviousStep.addEventListener("click", () => {
+        let { currentStep, lastStep } = getSteps();
+
+        if (currentStep != 1) {
+            currentStep -= 1;
+            instructionsTextElement.textContent = instructions[currentStep - 1]
+            stepFractionPresentation.textContent = `${currentStep} / ${lastStep}`;
+            stepCounter.textContent = currentStep;
+            progressBar.value = currentStep / lastStep;
+
+            if (currentStep == 1) {
+                getPreviousStep.style.cursor = "not-allowed";
+                getPreviousStep.disabled = true;
+            }
+            getNextStep.disabled = false;
+            getNextStep.style.cursor = "pointer";
         }
-        getNextStep.disabled = false;
-        getNextStep.style.cursor = "pointer";
-    }
-})
+    })
 
-getNextStep.addEventListener("click", () => {
-    let { currentStep, lastStep } = getSteps();
+    getNextStep.addEventListener("click", () => {
+        let { currentStep, lastStep } = getSteps();
 
-    if (currentStep != lastStep) {
-        currentStep += 1;
-        stepFractionPresentation.textContent = `${currentStep} / ${lastStep}`;
-        stepCounter.textContent = currentStep;
-        progressBar.value = currentStep / lastStep;
+        if (currentStep != lastStep) {
+            currentStep += 1;
+            instructionsTextElement.textContent = instructions[currentStep - 1]
+            stepFractionPresentation.textContent = `${currentStep} / ${lastStep}`;
+            stepCounter.textContent = currentStep;
+            progressBar.value = currentStep / lastStep;
 
-        if (currentStep == lastStep) {
-            getNextStep.style.cursor = "not-allowed";
-            getNextStep.disabled = true;
+            if (currentStep == lastStep) {
+                getNextStep.style.cursor = "not-allowed";
+                getNextStep.disabled = true;
+            }
+            getPreviousStep.disabled = false;
+            getPreviousStep.style.cursor = "pointer";
         }
-        getPreviousStep.disabled = false;
-        getPreviousStep.style.cursor = "pointer";
-    }
-})
+    })
+}
+
+main()
