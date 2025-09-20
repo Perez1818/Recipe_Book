be73a6a5dc463be0366a172978af85d10a4bdd53
liamgines <109954426+liamgines@users.noreply.github.com>
2025-09-20 04:19:56 -0700

    Make function that can be used to create new upload restrictions

--- controllers/filesController.js
+++ controllers/filesController.js
@@ -1,11 +1,22 @@
-const upload = require("../middleware/fileUploader.js");
+const getCustomUpload = require("../middleware/fileUploader.js");
+
+const PARENT_DIRECTORY = __dirname;
+const UPLOADS_DIRECTORY = `${PARENT_DIRECTORY}/../public/uploads`;
+const BYTES_PER_MEGABYTE = 1024 * 1024;
+
+const ALLOWED_AVATAR_FILE_TYPES = ["png", "jpg", "jpeg"];
+const AVATAR_DIRECTORY = `${UPLOADS_DIRECTORY}/avatar`;
+const BYTES_PER_AVATAR = BYTES_PER_MEGABYTE;
+const AVATAR_FIELD_NAME = "avatar";
+
+const uploadSingleAvatar = getCustomUpload(ALLOWED_AVATAR_FILE_TYPES, AVATAR_DIRECTORY, BYTES_PER_AVATAR, AVATAR_FIELD_NAME);
 
 exports.getUploader = (request, response) => {
     response.render("upload");
 }
 
 exports.uploadAvatar = [
-    upload.single("avatar"),
+    uploadSingleAvatar,
 
     (request, response, next) => {
         if (request.file !== undefined) {
--- middleware/fileUploader.js
+++ middleware/fileUploader.js
@@ -2,39 +2,41 @@ const multer = require("multer");
 const mime = require("mime-types");
 const crypto = require("crypto");
 
-const PARENT_DIRECTORY = __dirname;
-const BYTES_PER_MEGABYTE = 1024 * 1024;
 const NUM_FILES_ACCEPTED = 1;
 
-const fileFilter = (request, file, callback) => {
-    const allowedFileTypes = ["png", "jpg", "jpeg"];
-    const extension = mime.extension(file.mimetype);
-    if (allowedFileTypes.includes(extension)) {
-        callback(null, true);
+function getCustomUpload(allowedFileTypes, destinationFolder, bytesPerUpload, fieldName) {
+    const fileFilter = (request, file, callback) => {
+        const extension = mime.extension(file.mimetype);
+        if (allowedFileTypes.includes(extension)) {
+            callback(null, true);
+        }
+        else {
+            callback(new Error(`Only ${allowedFileTypes.join(", ")} are allowed.`));
+        }
     }
-    else {
-        callback(new Error(`Only ${allowedFileTypes.join(", ")} are allowed.`));
-    }
+
+    const storage = multer.diskStorage({
+        destination: destinationFolder,
+
+        /* https://github.com/expressjs/multer/blob/main/storage/disk.js */
+        filename: (request, file, callback) => {
+            crypto.randomBytes(16, (error, raw) => {
+                const extension = mime.extension(file.mimetype);
+                const fileName = raw.toString("hex") + "." + extension;
+                callback(error, error ? undefined : fileName)
+            })
+        }
+    });
+
+    const limits = { fileSize: bytesPerUpload, files: NUM_FILES_ACCEPTED };
+
+    const upload = multer({ 
+        storage: storage,
+        limits: limits,
+        fileFilter: fileFilter
+    });
+    
+    return upload.single(fieldName);
 }
 
-const storage = multer.diskStorage({
-    destination: `${PARENT_DIRECTORY}/../public/uploads/avatar`,
-    /* https://github.com/expressjs/multer/blob/main/storage/disk.js */
-    filename: (request, file, callback) => {
-        crypto.randomBytes(16, (error, raw) => {
-            const extension = mime.extension(file.mimetype);
-            const fileName = raw.toString("hex") + "." + extension;
-            callback(error, error ? undefined : fileName)
-        })
-    }
-});
-
-const limits = { fileSize: BYTES_PER_MEGABYTE, files: NUM_FILES_ACCEPTED };
-
-const upload = multer({ 
-    storage: storage,
-    limits: limits,
-    fileFilter: fileFilter
-});
-
-module.exports = upload;
+module.exports = getCustomUpload;
