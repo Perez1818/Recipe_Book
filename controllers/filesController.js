21f44cfebf037525ce5b0946f6ec7cca4da80323
liamgines <109954426+liamgines@users.noreply.github.com>
2025-09-20 02:25:29 -0700

    Stop saving unrecognized file types

--- controllers/filesController.js
+++ controllers/filesController.js
@@ -1,6 +1,35 @@
 const multer = require("multer");
 const PARENT_DIRECTORY = __dirname;
-const upload = multer({ dest: `${PARENT_DIRECTORY}/../public/uploads/` });
+const mime = require("mime-types");
+
+const BYTES_PER_MEGABYTE = 1024 * 1024;
+const NUM_FILES_ACCEPTED = 1;
+
+const storage = multer.diskStorage({
+    destination: `${PARENT_DIRECTORY}/../public/uploads/avatar`,
+    filename: (request, file, callback) => {
+        const extension = mime.extension(file.mimetype);
+        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1E9);
+        callback(null, file.fieldname + "-" + uniqueSuffix + "." + extension);
+    }
+});
+
+const fileFilter = (request, file, callback) => {
+    const allowedFileTypes = ["png", "jpg", "jpeg"];
+    const extension = mime.extension(file.mimetype);
+    if (allowedFileTypes.includes(extension)) {
+        callback(null, true);
+    }
+    else {
+        callback(new Error(`Only ${allowedFileTypes.join(", ")} are allowed.`));
+    }
+}
+
+const upload = multer({ 
+    storage: storage,
+    limits: { fileSize: BYTES_PER_MEGABYTE, files: NUM_FILES_ACCEPTED },
+    fileFilter: fileFilter
+});
 
 exports.getUploader = (request, response) => {
     response.render("upload");
