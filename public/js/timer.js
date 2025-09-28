85954073fea4f3d6d8b62e864459ef68d493179a
Ty <168314093+tyreseGill@users.noreply.github.com>
2025-09-28 10:30:20 -0700

    feat(time-steps): smooth animation + date reliant - Progress bar for timer has an FPS of 20 frames per second. - Makes alarm dependent on Date objects instead of being dependent on the number of elapsed seconds.     - Elapsed seconds was not reliable due to     the fact that it only counts down when the     computer is awake, thereby pausing the timer. - Stops flashing of timer after 3 seconds.

--- public/js/timer.js
+++ public/js/timer.js
@@ -5,12 +5,14 @@ const timerBar = document.getElementById("timer");
 
 // Time variables
 let minutes = 5;
-secondsElapsed = 0;
 let secondsToRun = minutes * 60;
 const SECOND = 1_000;
+let msToRun = secondsToRun * 1_000;
+const targetTime = Date.now() + msToRun;
 
-// Assigns ID to be used to stop calling updateTimer every second
+// Assigns rate at which to call updateTimer and updateTimerBar and the IDs to stop calls
 timerId = setInterval(updateTimer, SECOND);
+timerBarId = setInterval(updateTimerBar, SECOND / 20);
 
 // Returns unit of time with extra "0" if it consists of one digit
 function padTime(timeUnit) {
@@ -31,12 +33,11 @@ function flashTimer() {
 
 // Updates the time of the timer every second until time runs out
 function updateTimer() {
-    secondsElapsed++;
-    // Updates progress bar for elapsed time
-    timerBar.value = secondsElapsed / secondsToRun;
-   
+    remainingTimeMs = targetTime - Date.now();
+    remainingTimeS = remainingTimeMs / 1_000;
+
     // Calculates minutes and seconds
-    const minutesWithDecimal = (secondsToRun - secondsElapsed) / 60;
+    const minutesWithDecimal = (remainingTimeS) / 60;
     minutes = Math.trunc(minutesWithDecimal)
     seconds = Math.round((minutesWithDecimal - minutes) * 60, 2);
    
@@ -46,8 +47,21 @@ function updateTimer() {
     visibleCountdown.textContent = `${minutes}:${seconds}`
 
     // Stops timer when time runs out and makes text flash
-    if (secondsElapsed === secondsToRun) {
+    if (remainingTimeS <= 0) {
         clearInterval(timerId);
-        alarmOffId = setInterval(flashTimer, SECOND / 4)
+        clearInterval(timerBarId);
+
+        // Plays alarm for 3 seconds before stopping
+        let alarmOffId;
+        alarmOffId = setInterval(flashTimer, SECOND / 4);
+        setTimeout(function() {
+            clearInterval(alarmOffId);
+        }, 3_000);
     }
+}
+
+// Updates progress bar animation to be much smoother using ms
+function updateTimerBar() {
+    remainingTimeMs = targetTime - Date.now();
+    timerBar.value = remainingTimeMs / msToRun;
 }
