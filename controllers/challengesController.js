c516666375295680a98ac0ee5468183bdad8088d
liamgines <109954426+liamgines@users.noreply.github.com>
2025-11-08 06:00:57 -0800

    Allow users to create challenges

--- controllers/challengesController.js
+++ controllers/challengesController.js
@@ -1,4 +1,5 @@
 const { getSingleUpload } = require("../middleware/fileUploader.js");
+const challengesTable = require("../database/challengesTable.js");
 
 const PARENT_DIRECTORY = __dirname;
 
@@ -14,8 +15,18 @@ exports.getChallengeMaker = async (request, response) => {
 
 exports.createChallenge = async (request, response) => {
     uploadSingleThumbnail(request, response, async (error) => {
+        try {
+            await challengesTable.createChallenge(request.user.id, request.body.title, request.body.description, request.file.filename, 
+                                                  request.body.start, request.body.cutoff, request.body.points, 
+                                                  request.body["required-ingredients"], request.body["max-ingredients"]);
+            response.status(201).send("Challenge creation succeeded.");
+        }
+
+        catch (error) {
+            response.status(400).send("Challenge creation failed.");
+        }
+
         console.log(request.file);
         console.log(request.body);
-        response.status(501).send("Creating a challenge is not implemented yet.");
     });
 };
