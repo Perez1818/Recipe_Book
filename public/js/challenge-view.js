7cf75080dcbbab95f3f3c384c607b000dc76cc75
Ty <168314093+tyreseGill@users.noreply.github.com>
2025-11-11 21:27:20 -0800

    feat: load user recipes & add participation flow - Display user's recipes as images in challenge section - Gray out recipes that don’t meet ingredient requirements - Update participate button states (red -> participating, green -> complete) - Add newspaper SVG link to jump to challenge details - Show/hide “Select One of Your Recipes to Satisfy the Challenge” section based on participation status

--- public/js/challenge-view.js
+++ public/js/challenge-view.js
@@ -1,4 +1,6 @@
-import { getUsername } from "./users.js";
+import { getUsername, getCurrentUserDetails } from "./users.js";
+import { getRecipesByUser } from "./recipes.js";
+
 
 // DOM elements to fill in
 const challengeTitle = document.getElementById("challenge-name");
@@ -13,6 +15,8 @@ const maxIngredientsEl = document.getElementById("max-ingredients");
 const numParticipantsEl = document.getElementById("num-participants");
 const participateButton = document.getElementById("participate-button");
 const timeLeftElements = Array.from(document.getElementsByClassName("time-left"));
+const userRecipesContainer = document.getElementById("user-recipe-container");
+const submissionArea = document.getElementById("submission-section");
 
 // Gets details based on the id parameter in URL
 async function getChallenge(id) {
@@ -25,12 +29,96 @@ async function getChallenge(id) {
     }
 }
 
+function normalizeIngredientName(ingredientName) {
+    if (typeof ingredientName !== "string") return ""; // safeguard
+    ingredientName = ingredientName.toLowerCase();
+    ingredientName = ingredientName.replace(/s$/, '');
+    return ingredientName;
+}
+
+function preventRecipeFromBeingClicked(img) {
+    img.classList.add("gray-out");
+    img.style.cursor = "not-allowed";
+    img.disabled = true;
+}
+
 async function main() {
     // Obtains variables associated with the given challenge
     const params = new URLSearchParams(window.location.search);
     const challengeId = params.get("id");
     const challenge = await getChallenge(challengeId);
     let { user_id, title, thumbnail, description, start, cutoff, points, required_ingredients, max_ingredients } = challenge;
+
+
+    //  Obtains all the recipes created by currently registered user
+    let userRecipes;
+    try {
+        const currentUser = await getCurrentUserDetails();
+        const currentUserId = currentUser.id;
+        const userRecipesResult = await getRecipesByUser(currentUserId);
+        // 
+        const userRecipes = userRecipesResult.recipes;
+        const userRecipeIngredients = userRecipesResult.ingredients;
+        console.log("userRecipes:", userRecipes)
+
+        let recipeIngredients = {};
+
+        // Builds dictionary of ingredients associated with each recipe ID
+        for (const ingredient of userRecipeIngredients) {
+            const recipeId = ingredient.recipe_id;
+            const ingredientName = normalizeIngredientName(ingredient.name);
+            if (recipeIngredients[recipeId]) {
+                recipeIngredients[recipeId].push(ingredientName);
+            }
+            else {
+                recipeIngredients[recipeId] = [ingredientName];
+            }
+        }
+
+        console.log("ingredients:", recipeIngredients)
+
+        // Populate section with list of user's recipes
+        for (let i=0; i < userRecipes.length; i++) {
+            const recipeId = userRecipes[i].id;
+            const ingredients = recipeIngredients[recipeId] || [];
+            const numIngredients = ingredients.length;
+
+            const img = document.createElement("img");
+            const thumbnail = userRecipes[i].thumbnail;
+
+            img.src = `uploads/multimedia/${thumbnail}`;
+            img.title = userRecipes[i].name;
+            img.recipeId = recipeId;
+            userRecipesContainer.appendChild(img);
+
+            // Recipes with 0 ingredients should not be submitted
+            if (numIngredients === 0) {
+                preventRecipeFromBeingClicked(img);
+            }
+
+            // Recipes with more ingredients than specified should not be submitted
+            if (max_ingredients) {
+                if (numIngredients > max_ingredients) {
+                    preventRecipeFromBeingClicked(img);
+                }
+            }
+            
+            // Recipes that are missing one of the required ingredients should not be submitted
+            if (required_ingredients[0]) {
+                for (let j=0; j < required_ingredients.length; j++) {
+                    const requiredIngredient = normalizeIngredientName(required_ingredients[j]);
+                    if (!ingredients.includes(requiredIngredient)) {
+                        preventRecipeFromBeingClicked(img);
+                    }
+                }
+            }
+        }
+    } catch (err) {
+        console.error(err)
+    }
+
+    // Fetches all images that have not been grayed out
+    const satisfiableUserRecipes = Array.from(userRecipesContainer.querySelectorAll(":not(img.gray-out)"));
 
     // Prevents user from attempting to participate in challenge that has already expired
     if (new Date(cutoff) < new Date()) {
@@ -95,7 +183,7 @@ async function main() {
     // Fills in username, description and points
     usernameEl.textContent = await getUsername(user_id);
     descriptionEl.textContent = `${description}`;
-    numPointsEl.textContent = `${points} pts`;
+    numPointsEl.textContent = `${points} pts 🎯`;
 
     // Adds bullets to display required ingredients if there are any
     if (required_ingredients[0]) {
@@ -111,23 +199,56 @@ async function main() {
         maxIngredientsEl.textContent += max_ingredients;
     }
     else {
-        maxIngredientsEl.remove(); // Removes element
+        maxIngredientsEl.remove();
     }
+
+    // Allows the user recipes that are qualified to be clickable until one is chosen
+    satisfiableUserRecipes.forEach(
+        (userRecipe) => {
+            userRecipe.addEventListener(
+                "click", () => {
+                    console.log(userRecipe.recipeId, "added!");
+                    participateButton.textContent = " Completed";
+                    participateButton.disabled = true;
+                    participateButton.style.cursor = "not-allowed";
+                    participateButton.style.backgroundColor = "lightgreen";
+                    submissionArea.style.display = "none";
+
+                    satisfiableUserRecipes.forEach(img => {
+                        preventRecipeFromBeingClicked(img)
+                    });
+                }
+            )
+        }
+    )
 
     participateButton.addEventListener(
         "click", (event) => {
             event.preventDefault();
-            event.stopPropagation();    // stop event from reaching parent elements
+            event.stopPropagation();    // Stops event from reaching parent elements
             participateButton.classList.toggle("participating");
             const numParticipants = parseInt(numParticipantsEl.textContent);
 
+            // Updates participant stats for challenge updates database
             if (participateButton.classList.contains("participating")) {
                 participateButton.textContent = " Participating ✓";
+                participateButton.style.backgroundColor = "salmon";
                 numParticipantsEl.textContent = `${numParticipants + 1} 👤`;
+
+                // Displays submission area for participant
+                submissionArea.style.display = "flex";
+                // Scrolls the submission area into view
+                submissionArea.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
             }
             else {
                 participateButton.textContent = " Participate";
+                participateButton.style.backgroundColor = "lightgray";
                 numParticipantsEl.textContent = `${numParticipants - 1} 👤`;
+                
+                submissionArea.style.display = "none";
             }
         }
     );
