8429501f9fbaa08389dd51d086c4b7e80e452fb6
liamgines <109954426+liamgines@users.noreply.github.com>
2025-11-29 20:01:43 -0800

    Add case-insensitive query for recipes

--- database/recipesTable.js
+++ database/recipesTable.js
@@ -5,7 +5,7 @@ const PARENT_DIRECTORY = __dirname;
 dotenv.config({ path: `${PARENT_DIRECTORY}/../.env` });
 
 async function getRecipesByTagOrCountry(tag, country) {
-    const { rows } = await pool.query(`SELECT * FROM recipes WHERE ($1 = ANY(tags) OR $2 = ANY(tags));`, [tag, country]);
+    const { rows } = await pool.query(`SELECT * FROM recipes WHERE ($1 ILIKE ANY(tags) OR $2 ILIKE ANY(tags));`, [tag, country]);
     return rows;
 }
 
